9d7387af1a045ae2112344d2025ec71b7d87cb70
sachin diwakar <sachindiwakar1802@gmail.com>
2026-02-16 23:13:29 +0530

    code

--- config/controller/authController.js
+++ config/controller/authController.js
@@ -104,8 +104,23 @@ export const logout = async (req, res) => {
 
 export const googleLogin = async (req,res) =>{
   try {
-    let {name,email} = 
+    let {name,email} = req.body;
+    let user = await User.findOne({email})
+    if(!user){
+      user = await User.create({
+        name,email
+      })
+      let token = await genToken(user._id)
+      res.cookie("token",token,{
+        httpOnly:true,
+        secure:false,
+        sameSite:"Strict",
+        maxAge:7*24*60*60*1000
+      })
+      return res.status(201).json(user)
+    }
   } catch (error) {
-    
+    console.log("googleLogin error")
+    return res.status(500).json({message:`Google Login error ${error}`})
   }
 }
