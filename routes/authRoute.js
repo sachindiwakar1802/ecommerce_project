96f474264f460163f2dda3fa7e3e8080912f436a
sachin diwakar <sachindiwakar1802@gmail.com>
2026-02-16 23:16:43 +0530

    codde

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,14 @@
 import express from "express"
 
-import { login, logout, register } from "../config/controller/authController.js"
+import { login, logout, register,googleLogin } from "../config/controller/authController.js"
 
 const authRoute = express.Router()
 
 authRoute.post("/register",register)
 authRoute.post("/login",login)
 authRoute.get("/logout",logout)
+
+
+authRoute.post("/googlelogin",googleLogin)
+
 export default authRoute
